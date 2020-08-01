b1e6798bc653d0caf2d01425c4d798fa86f7a2f5
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-01 09:39:26 -0700

    Add route to notes model api

--- server/api-routes/mysql.js
+++ server/api-routes/mysql.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const db = require('../models/notes')
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('hello world');
+router.get('/', function(req, res) {
+  res.send({'message': "hello"});
 });
+
+router.get('/notes', db.getNotes);
 
 module.exports = router;

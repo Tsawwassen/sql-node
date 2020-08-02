fb10179e49fa2e4070fcafdbead27117d6e4386b
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-02 07:33:45 -0700

    Use DB config / connection object from config folder

--- server/models/notes.js
+++ server/models/notes.js
@@ -1,15 +1,7 @@
-//mySQL Database init
-var mysql = require('mysql');
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "node",
-  password: "Tsawwassen604",
-  port: 3306,
-});
+db = require('../config/db-config');
 
 const getNotes = (req, res) => {
-	
-    con.query(`SELECT * FROM ${mysql.escapeId('notes.notes')};`, function (err, result) {
+    db.con.query(`SELECT * FROM ${db.mysql.escapeId('notes.notes')};`, function (err, result) {
     	if (err) throw err;
     	res.send({'message': "Result: " + result[0].note });
   	});

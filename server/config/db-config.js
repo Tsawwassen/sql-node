8cfdf0417febee0cdc72db6a6e016181bf29ce6e
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-02 07:33:08 -0700

    Add config folder to store DB config / connection object

---
+++ server/config/db-config.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "node",
+  password: "Tsawwassen604",
+  port: 3306,
+});
+
+module.exports = {
+  mysql,
+  con
+}

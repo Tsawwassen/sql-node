d736be413cf6e7b654f35b93f6ff17203ba61360
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-02 08:20:39 -0700

    Add API to add notes to the database, and change what database the get all query is looking at

--- server/models/notes.js
+++ server/models/notes.js
@@ -1,13 +1,27 @@
-db = require('../config/db-config');
+const db = require('../config/db-config');
 
 const getNotes = (req, res) => {
-    db.con.query(`SELECT * FROM ${db.mysql.escapeId('notes.notes')};`, function (err, result) {
+    db.con.query(`SELECT * FROM ${db.mysql.escapeId('comment.comment')};`, function (err, result) {
     	if (err) throw err;
-    	res.send({'message': "Result: " + result[0].note });
+    	res.send({'message': "Result: " + JSON.stringify(result)});
   	});
 
 }
 
+const insertNotes = (req, res) => {
+
+	let insertQuery = 'INSERT INTO ?? (??,??) VALUES (?,?)';
+    let query = db.mysql.format(insertQuery,["comment.comment","text","date","TESTING INSERT", (( new Date() ).toLocaleDateString().split("/")) ]);
+
+    db.con.query(query,(err, response) => {
+        if (err) throw res.send({'message': err});
+
+        // rows added
+        res.send({'message': "inserted" });
+    });
+}
+
 module.exports = {
   getNotes,
+  insertNotes,
 }

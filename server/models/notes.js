c66e453c12789220d0367cc9e9275087fc61beb7
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-03 07:27:51 -0700

    Use stored procedures to interact with the database

--- server/models/notes.js
+++ server/models/notes.js
@@ -1,7 +1,11 @@
 const db = require('../config/db-config');
 
 const getNotes = (req, res) => {
-    db.con.query(`SELECT * FROM comment;`, function (err, result) {
+
+    //var sql =`SELECT * FROM comment;`
+    var sql = `CALL GetAllComments()`
+
+    db.con.query(sql, function (err, result) {
     	if (err) throw err;
     	res.send({'message': "Result: " + JSON.stringify(result)});
   	});
@@ -10,12 +14,14 @@ const getNotes = (req, res) => {
 
 const insertNotes = (req, res) => {
 
-        let textInput = "TESTING INSERT";
+        let textInput = "!!TESTING INSERT!!";
         let dateInput = new Date().toLocaleDateString().split("/");
 
         //Note, the values must be wrapped in single quotes. If they are not, you will receive an SQL syntax error when running the command.
-        var sql = `INSERT INTO comment (text, date) VALUES ('${textInput}', '${dateInput}');`;
-        
+        //var sql = `INSERT INTO comment (text, date) VALUES ('${textInput}', '${dateInput}');`;
+        var sql = `CALL InsertComment('${textInput}', '${dateInput}')`
+
+
         db.con.query(sql, function (err, result) {
             if (err) throw res.send({'message': err});
             res.send({'message': "inserted" });

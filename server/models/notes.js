2abe3f64a5f1a97ea6b2a660dc0d011dea5c91d5
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-03 06:46:23 -0700

    Clean up SQL code to be easier to read

--- server/models/notes.js
+++ server/models/notes.js
@@ -1,7 +1,7 @@
 const db = require('../config/db-config');
 
 const getNotes = (req, res) => {
-    db.con.query(`SELECT * FROM ${db.mysql.escapeId('comment.comment')};`, function (err, result) {
+    db.con.query(`SELECT * FROM comment;`, function (err, result) {
     	if (err) throw err;
     	res.send({'message': "Result: " + JSON.stringify(result)});
   	});
@@ -10,15 +10,17 @@ const getNotes = (req, res) => {
 
 const insertNotes = (req, res) => {
 
-	let insertQuery = 'INSERT INTO ?? (??,??) VALUES (?,?)';
-    let query = db.mysql.format(insertQuery,["comment.comment","text","date","TESTING INSERT", (( new Date() ).toLocaleDateString().split("/")) ]);
+        let textInput = "TESTING INSERT";
+        let dateInput = new Date().toLocaleDateString().split("/");
 
-    db.con.query(query,(err, response) => {
-        if (err) throw res.send({'message': err});
-
-        // rows added
-        res.send({'message': "inserted" });
-    });
+        //Note, the values must be wrapped in single quotes. If they are not, you will receive an SQL syntax error when running the command.
+        var sql = `INSERT INTO comment (text, date) VALUES ('${textInput}', '${dateInput}');`;
+        
+        db.con.query(sql, function (err, result) {
+            if (err) throw res.send({'message': err});
+            res.send({'message': "inserted" });
+        });
+ 
 }
 
 module.exports = {
